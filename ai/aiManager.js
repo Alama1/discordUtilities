1a361daa30738277810b48b08dfb7bc3c62fd47e
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2023-06-10 16:56:20 +0300

    fixed bug where 2 people asking crush the bot

--- ai/aiManager.js
+++ ai/aiManager.js
@@ -4,14 +4,19 @@ const characterAI = new CharacterAI();
 class aiManager {
     constructor(app) {
         this.app = app
+        this.inProcess = false
     }
     async init() {
         await characterAI.authenticateWithToken(this.app.config.properties.ai.token);
     }
     async getMessageFromAi(message) {
+        if(this.inProcess) return { text: 'Работаю над другим вопросом, подожди.' }
+        this.inProcess = true
         const characterId = this.app.config.properties.ai.characterId
         const chat = await characterAI.createOrContinueChat(characterId);
-        return await chat.sendAndAwaitResponse(message, true)
+        const reply = await chat.sendAndAwaitResponse(message, true)
+        this.inProcess = false
+        return reply
     }
 }
 
