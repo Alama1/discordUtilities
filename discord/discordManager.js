35a4e830ef17204618b4b13e13775d2e68b3c5f5
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2023-06-28 21:41:14 +0300

    Fixed bug with node-schedule of something

--- discord/discordManager.js
+++ discord/discordManager.js
@@ -1,6 +1,5 @@
 const {GatewayIntentBits, Client, EmbedBuilder} = require('discord.js')
 const InteractionHandler = require('./handlers/interactionHandler')
-const schedule = require('node-schedule')
 
 class DiscordManager {
     constructor(app) {
@@ -19,8 +18,10 @@ class DiscordManager {
         this.client.on('ready', () => {
             console.log(`${this.client.user.tag} is ready!`)
             this.interactionHandler = new InteractionHandler(this)
-            this.watchAvatars()
-            schedule.scheduleJob('*/1 * * * *', () => { this.watchAvatars() })
+
+            setInterval(async () => {
+                await this.watchAvatars()
+            }, 60000)
         })
 
         this.client.on('interactionCreate', interaction => {
@@ -35,23 +36,22 @@ class DiscordManager {
 
     }
     async watchAvatars() {
-        const guild = await this.client.guilds.fetch(this.app.config.properties.discord.guildID)
         const usersToCheck = this.app.config.properties.avatarWatchList
         const userIDs = Object.keys(usersToCheck)
         console.log(`Checking avatars ${Date.now().toString()} for users: ${userIDs.join(' ')}`)
-        for (let user of userIDs) {
-            const currentUser = (await guild.members.fetch(user)).user
-            const oldAV = usersToCheck[user].avatar
+        for (let userID of userIDs) {
+            const currentUser = await this.client.users.fetch(userID)
+            const oldAV = usersToCheck[userID].avatar
 
             if (currentUser.avatar === oldAV) continue
 
-            this.app.config.properties.avatarWatchList[user] = { avatar: currentUser.avatar }
+            this.app.config.properties.avatarWatchList[userID] = { avatar: currentUser.avatar }
             this.app.config.saveAvatarsToConfig()
-            const newAVURL = (await guild.members.fetch(user)).displayAvatarURL({ size: 4096, dynamic: true })
+            const newAVURL = `https://cdn.discordapp.com/avatars/${userID}/${currentUser.avatar}?size=4096`
             const channel = await this.client.channels.fetch(this.app.config.properties.discord.chatID)
             const avatarEmbed = new EmbedBuilder()
                 .setTitle(currentUser.username)
-                .setDescription(`<@${user}> изменил аватарку!`)
+                .setDescription(`<@${userID}> изменил аватарку!`)
                 .setImage(newAVURL)
                 .setColor('#2335ff');
             channel.send({ embeds: [avatarEmbed]})
