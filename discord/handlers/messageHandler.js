5abe52e08f1f42f0fe2680f04598b096b3ef776f
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2024-04-18 05:08:16 +0300

    Small feature added

--- discord/handlers/messageHandler.js
+++ discord/handlers/messageHandler.js
@@ -8,6 +8,11 @@ class MessageHandler {
         if (Math.random() > this.discord.app.config.properties.discord.reactionChance) {
             this.reactToMessage(message)
         }
+        if (message.content.toLowerCase().trim() == 'да') {
+            if (Math.random() > 0.7) {
+                message.reply('https://tenor.com/view/gifkusta-gif-24281133')
+            }
+        }
     }
 
     reactToMessage(message) {
