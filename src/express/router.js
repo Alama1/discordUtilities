e7fb48ddaee9667247d1e271c9aa4e3295db2630
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2024-06-07 03:14:47 +0300

    Error logging

--- src/express/router.js
+++ src/express/router.js
@@ -53,6 +53,7 @@ class expressRouter {
             res.status(200)
             res.send({success: true, message: 'Gif was deleted!'})
         } catch(e) {
+            console.log(e.message)
             res.status(400)
             res.send({ success: false, message: 'Something went wrong :(' })
         }
@@ -63,6 +64,7 @@ class expressRouter {
             res.status(200)
             res.send({ success: true, message: this.server.app.discord.users })
         } catch(e) {
+            console.log(e.message)
             res.status(500)
             res.send({ success: false, message: 'Something welt wrong...' })
         }
@@ -75,6 +77,7 @@ class expressRouter {
             res.status(200)
             res.send({ success: true, message: { gifChance, personalGifChance } })
         } catch(e) {
+            console.log(e.message)
             res.status(500)
             res.send({ success: false, message: 'Internal server error.' })
         }
@@ -98,6 +101,7 @@ class expressRouter {
             res.status(201)
             res.send({ success: true, message: 'Chance updated!' })
         } catch(e) {
+            console.log(e.message)
             res.status(500)
             res.send({ success: false, message: 'Internal server error.' })
         }
@@ -114,6 +118,7 @@ class expressRouter {
             res.status(200)
             res.send({ success: true, message: refactoredAvatars })
         } catch(e) {
+            console.log(e.message)
             res.status(500)
             res.send({ success: false, message: 'Internal server error.' })
         }
@@ -134,7 +139,7 @@ class expressRouter {
             res.status(200)
             res.send({ success: true, message: { global: gif, personal: restructuredPersonalGifs } })
         } catch(e) {
-            console.log(e)
+            console.log(e.message)
             res.status(500)
             res.send({ success: false, message: 'Internal server error.' })
         }
@@ -166,6 +171,7 @@ class expressRouter {
             res.status(201)
             res.send({ success: true, message: 'Reaction added successfully!' })
         } catch(e) {
+            console.log(e.message)
             res.status(500)
             res.send({ success: false, message: 'Something welt wrong...' })
         }
